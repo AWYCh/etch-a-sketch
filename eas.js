f6201abf3781f5631a8c0474d3745f82112b7c0d
AWYCh <AWYCh@users.noreply.github.com>
2024-04-19 15:10:28 +0800

    Create hover effect that leaves pixelated trail

--- eas.js
+++ eas.js
@@ -1,6 +1,8 @@
-//Create a webpage with a 16x16 grid of square divs.
-//Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
 //Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 const gridLength = 500;
 let rows = 16;
 let columns = 16;
@@ -8,16 +10,26 @@ let columns = 16;
 const grid = document.querySelector(".grid");
 grid.style.width = `${gridLength}px`;
 grid.style.height = `${gridLength}px`;
+grid.style.margin = "150px auto";
 
-function createGridSquare() {
+function changeColor (event) {
+    if(event.type === "mouseover" && !mouseDown) return
+    event.target.style.backgroundColor = "#000000";
+    }
+
+function createGrid() {
     for (let i=0; i<(rows*columns); i++) {
         const gridSquare = document.createElement("div");
-        gridSquare.style.width = `${(gridLength/rows)-2}px`;
-        gridSquare.style.height = `${(gridLength/columns)-2}px`;
+        gridSquare.style.width = `${(gridLength/rows)}px`;
+        gridSquare.style.height = `${(gridLength/columns)}px`;
+        gridSquare.style.backgroundColor = "#ffffff";
         gridSquare.classList.add("grid-square");
+        
+        gridSquare.addEventListener ("mouseover", changeColor);
+        gridSquare.addEventListener ("mousedown", changeColor);
         grid.appendChild(gridSquare);
     }
 }
 
-createGridSquare();
+createGrid();
 
