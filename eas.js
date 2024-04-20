ab738e0cf7aaac71bfb8302b8483930db68e9560
AWYCh <AWYCh@users.noreply.github.com>
2024-04-20 16:59:39 +0800

    Create button that allows user to set size of sketch grid

--- eas.js
+++ eas.js
@@ -1,27 +1,27 @@
-//Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
+//Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. 
+//Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
-const gridLength = 500;
-let rows = 16;
-let columns = 16;
+const gridLength = 700;
+//let inputSize = changeSize();
 
 const grid = document.querySelector(".grid");
 grid.style.width = `${gridLength}px`;
 grid.style.height = `${gridLength}px`;
-grid.style.margin = "150px auto";
+grid.style.margin = "25px auto";
 
 function changeColor (event) {
     if(event.type === "mouseover" && !mouseDown) return
     event.target.style.backgroundColor = "#000000";
     }
 
-function createGrid() {
-    for (let i=0; i<(rows*columns); i++) {
+function createGrid(size) {
+    for (let i=0; i<(size*size); i++) {
         const gridSquare = document.createElement("div");
-        gridSquare.style.width = `${(gridLength/rows)}px`;
-        gridSquare.style.height = `${(gridLength/columns)}px`;
+        gridSquare.style.width = `${(gridLength/size)}px`;
+        gridSquare.style.height = `${(gridLength/size)}px`;
         gridSquare.style.backgroundColor = "#ffffff";
         gridSquare.classList.add("grid-square");
         
@@ -31,5 +31,23 @@ function createGrid() {
     }
 }
 
-createGrid();
+createGrid(16);
+
+function removeGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function changeSize() {
+    let input = prompt("Enter number of squares per side");
+    let error = document.querySelector("#input-error");
+     if (input > 1 && input <= 100) {
+        removeGrid();
+        createGrid(input);
+        error.innerHTML = "";
+     } else {
+        error.innerHTML = "Please enter a number between 1 and 101";
+     }
+}
 
