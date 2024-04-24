afac89e4eb99820a1cc54174e6b4d50a859bd615
AWYCh <AWYCh@users.noreply.github.com>
2024-04-24 14:59:55 +0800

    Add clear button that allows current sketch area to be reset to white

--- eas.js
+++ eas.js
@@ -1,16 +1,23 @@
-//Randomize the squares’ RGB values with each interaction.
-//Implement a progressive darkening effect where each interaction darkens the square by 10%. The objective is to achieve a completely black square only ten interactions.
+//Implement a progressive darkening effect where each interaction darkens the square by 10%. 
+//The objective is to achieve a completely black square only ten interactions.
+const sizeButton = document.querySelector("#size");
+const blackButton = document.querySelector("#defaultColor");
+const randomButton = document.querySelector("#randomColor");
+const darkenButton = document.querySelector("#darken");
+const eraserButton = document.querySelector("#eraser");
+const clearButton = document.querySelector("#clear");
+
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 const gridLength = 600;
 let activeColor = 'black'
+let activeSize = 16;
 
 const grid = document.querySelector(".grid");
 grid.style.width = `${gridLength}px`;
 grid.style.height = `${gridLength}px`;
-grid.style.margin = "25px auto";
 
 function random(number) {
     return Math.floor(Math.random()*number);
@@ -31,11 +38,6 @@ function changeColor (event) {
         }  
 }
 
-function changeRandom (event) {
-    if(event.type === "mouseover" && !mouseDown) return
-    event.target.style.backgroundColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-    }
-
 function createGrid(size) {
     for (let i=0; i<(size*size); i++) {
         const gridSquare = document.createElement("div");
@@ -49,8 +51,6 @@ function createGrid(size) {
         grid.appendChild(gridSquare);
     }
 }
-
-createGrid(16);
 
 function removeGrid() {
     while (grid.firstChild) {
@@ -66,9 +66,21 @@ function changeSize() {
         createGrid(input);
         area.textContent = `Sketch area is ${input} x ${input}`;
         area.style.color = "#000000";
+        activeSize = input;
      } else {
         area.textContent = "Please enter a number between 1 and 101";
         area.style.color = "#ff0000";
      }
 }
 
+function clearGrid() {
+    removeGrid();
+    createGrid(activeSize);
+}
+
+createGrid(activeSize);
+sizeButton.addEventListener("click", changeSize);
+blackButton.addEventListener("click", () => {activateColor('black')});
+randomButton.addEventListener("click", () => {activateColor('random')});
+eraserButton.addEventListener("click", () => {activateColor('white')});
+clearButton.addEventListener("click", clearGrid);
